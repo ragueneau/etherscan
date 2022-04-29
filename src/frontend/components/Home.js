ff87e469089eecbd9617b6438b341e689f5ffcd1
Jocelyn <82773188+ragueneau@users.noreply.github.com>
2022-04-28 23:49:29 -0400

    Update Home.js

--- src/frontend/components/Home.js
+++ src/frontend/components/Home.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import { ethers } from "ethers"
-import { Row, Col, Card, Button } from 'react-bootstrap'
+import { Row, Col, Card } from 'react-bootstrap'
 import { Link } from "react-router-dom";
 
 const Home = ({ networkName }) => {
@@ -14,25 +14,69 @@ const Home = ({ networkName }) => {
     const getLastBlockNumber = async () => {
         const provider = new ethers.providers.Web3Provider(window.ethereum)
         const blockNumber = await provider.getBlockNumber()
-
         console.log('Last block number:', blockNumber)
+
         setLastBlockNumber(blockNumber)
     }
 
+    //const getLastBlockTransaction = async () => {
+    //    const provider = new ethers.providers.Web3Provider(window.ethereum)
+      //  const blockNumber = await provider.getBlockNumber()
+    //}
+
     useEffect(() => {
-        getLastBlockNumber()
+      getLastBlockNumber()
+
       }, [])
       if (loading) return (
         <main style={{ padding: "1rem 0" }}>
           <h2>Loading home...</h2>
         </main>
       )
-    // Render ---------------------------------------------------------------------------------------------------------- //
-    return (
+
+      //setLoading(false)
+      // Render ---------------------------------------------------------------------------------------------------------- //
+      return (
         <div className="flex justify-center">
             <div className="px-5 py-3 container">
                 <h2>Ethereum Block Scanner</h2>
-                {lastBlockNumber}
+
+                <Link to={`/block/${lastBlockNumber}`}>{lastBlockNumber}</Link>
+
+                <Row className="justify-content-center">
+                    <Col xs={1} md={4} lg={6}>
+                        <Card className="text-center">
+                            <Card.Body>
+                                <Card.Title>
+                                    <h3>Latest Blocks</h3>
+                                </Card.Title>
+                                <Card.Text>
+                                    <span className="text-muted">
+                                        <i className="fas fa-user-circle">Text</i>
+                                    </span>
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col xs={1} md={4} lg={6}>
+                        <Card className="text-center">
+                            <Card.Body>
+                                <Card.Title>
+                                    <h3>Latest Transactions</h3>
+
+                                </Card.Title>
+                                <Card.Text>
+                                    <span className="text-muted">
+                                        <i className="fas fa-user-circle">Text</i>
+                                    </span>
+
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+
+                </Row>
+
             </div>
         </div>
     );
