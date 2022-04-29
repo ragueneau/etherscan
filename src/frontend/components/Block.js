5140c9ad42fe115c631b1e29424ab8b74b2477c5
Jocelyn <82773188+ragueneau@users.noreply.github.com>
2022-04-28 23:49:39 -0400

    Create Block.js

---
+++ src/frontend/components/Block.js
@@ -0,0 +1,115 @@
+import { useState, useEffect } from 'react'
+import { ethers } from "ethers"
+import { Row, Col, Card, Button } from 'react-bootstrap'
+import { Link } from "react-router-dom";
+//import { HDWalletProvider } from '@truffle/hdwallet-provider';
+// import truffle wallet 'truffle-hdwallet-provider'
+
+
+
+
+//const provider = new HDWalletProvider(
+ //   'engine amazing run phrase help age detect fan charge approve border salute',
+//    'https://rinkeby.infura.io/v3/7b16e033fae342cd8afa67a9d340aaac'
+//);
+
+const Block = ({ blockNumber, networkName }) => {
+    const [loading, setLoading] = useState(false)
+    const [blockContent, setBlockContent] = useState([{
+        blockNumber: 1,
+        blockHash: '',
+        blockTransactions: [],
+    }])
+
+    blockNumber = blockNumber || 72533
+
+    //const [account, setAccount] = useState([])
+    console.log('Network:', networkName)
+
+    //get last block number
+    const getBlockNumber = async () => {
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+
+
+        const blockTransactions = await provider.getBlockWithTransactions(blockNumber)
+        console.log('block number requested:', blockNumber)
+        console.log('Block Transactions', blockTransactions)
+
+        setBlockContent(blockTransactions)
+    }
+
+    useEffect(() => {
+        getBlockNumber()
+
+    }, [])
+    if (loading) return (
+      <main style={{ padding: "1rem 0" }}>
+        <h2>Loading block {blockNumber}...</h2>
+      </main>
+    )
+
+    // Render ---------------------------------------------------------------------------------------------------------- //
+    return (
+        <div className="flex justify-center">
+            <div className="px-5 py-3 container text-center">
+                <h3>Block #{blockContent.number}</h3>
+
+                <Row className="justify-content-center">
+                    <Col xs={1} md={8} lg={12}>
+                        <Card className="text-center">
+                            <Card.Body>
+                                <Card.Title>
+                                    <h3>Latest Blocks</h3>
+                                </Card.Title>
+                                <Card.Text>
+                                    <div className="text-muted text-center">
+                                        Block height: <i className="fas fa-user-circle">{blockContent.number}</i> <Link to={`/block/${blockContent.number-1}`}>
+                                            <Button variant="primary" size="sm" className="ml-2">Previous</Button>
+                                        </Link> <Link to={`/block/${blockContent.number+1}`}>
+                                            <Button variant="primary" size="sm" className="ml-2">Next</Button>
+                                        </Link>
+                                    </div>
+                                    <div className="text-muted text-center">
+                                        Timestamp: <i className="fas fa-user-circle">{blockContent.timestamp}</i>
+                                    </div>
+
+                                    <div className="text-muted text-center">
+                                    </div>
+                                    <div className="text-muted text-center">
+                                        Burned Fees: <i className="fas fa-user-circle">{blockContent.timestamp}</i>
+                                    </div>
+                                    <div className="text-muted text-center">
+                                        Total Difficulty: <i className="fas fa-user-circle">{blockContent.difficulty}</i>
+                                    </div>
+                                    <div className="text-muted text-center">
+                                        Nonce: <i className="fas fa-user-circle">{blockContent.nonce}</i>
+                                    </div>
+                                    <div className="text-muted text-center">
+                                        Gas Used: <i className="fas fa-user-circle"></i>
+                                    </div>
+                                    <div className="text-muted text-center">
+                                        Gas Limit: <i className="fas fa-user-circle"></i>
+                                    </div>
+                                    <div className="text-muted text-center">
+                                        Miner: <Link to={`/address/${blockContent.miner}`}><i className="fas fa-user-circle">{blockContent.miner}</i></Link>
+                                    </div>
+                                    <div className="text-muted text-center">
+                                        Block hash: <Link to={`/block/${blockContent.hash}`}><i className="fas fa-user-circle">{blockContent.hash}</i></Link>
+                                    </div>
+                                    <div className="text-muted text-center">
+                                        Parent Hash: <Link to={`/block/${blockContent.parentHash}`}>
+                                        <i className="fas fa-user-circle">{blockContent.parentHash}</i>
+                                        </Link>
+                                    </div>
+
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+
+            </div>
+        </div>
+    );
+}
+export default Block
