f836cf4e07159e66e8429da3a7a93988bf288933
Jocelyn <82773188+ragueneau@users.noreply.github.com>
2022-05-06 15:55:29 -0400

    Update LatestTransactions.js

--- src/frontend/components/tx/LatestTransactions.js
+++ src/frontend/components/tx/LatestTransactions.js
@@ -7,7 +7,7 @@ const LatestTransactions = ({txs}) => {
         <ListGroup variant="flush">
             {txs.map((item, idx) => (
                 <ListGroup.Item key={idx}>
-                Tx <Link to={`/tx/${item.hash}`}>{item.hash.slice(0, 7) + '...'}</Link> {item.gas} xwei<br/>
+                Tx <Link to={`/tx/${item.hash}`}>{item.hash.slice(0, 7) + '...'}</Link> {item.gas} wei<br/>
                 From: <Link to={`/address/${item.from}`}>{item.from.slice(0, 9) + '...'+item.from.slice(33, 42)}</Link><br/>
                 {item.to ? <span>To: <Link to={`/address/${item.to}`}>{item.to.slice(0, 9) + '...'+item.to.slice(33, 42)}</Link><br/></span> : null}
                 </ListGroup.Item>
