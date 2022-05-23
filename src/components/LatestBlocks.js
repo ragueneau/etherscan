e8a78d5146a641a655239bcd8ff8b8685df39781
Jocelyn <82773188+ragueneau@users.noreply.github.com>
2022-05-23 04:22:20 -0400

    Update LatestBlocks.js

--- src/components/LatestBlocks.js
+++ src/components/LatestBlocks.js
@@ -1,26 +1,30 @@
 
 import React from "react";
-import { Card } from 'react-bootstrap'
+import { Row, Col, ListGroup } from 'react-bootstrap'
 import { Link } from "react-router-dom"
 
 const LatestBlocks = ({items}) => {
     return (
-        <Card className="text-center">
-            <Card.Body>
-                <Card.Text>
-                    <ul>
-                    {items.map((item, idx) => (
-                        <li key={idx} className="list-group-item">
-                        <Link to={`/block/${item.number}`}>{item.number}</Link> Hash: <Link to={`/block/${item.number}`}>{item.hash.slice(0, 15) + '...'}</Link><br/>
-                        {item.transactions.length} transaction(s)
-                        <br/>
-                        {item.timediff} secs ago
-                        </li>
-                    ))}
-                    </ul>
-                </Card.Text>
-            </Card.Body>
-        </Card>
+        <ListGroup variant="flush">
+            {items.map((item, idx) => (
+                <ListGroup.Item key={idx} className="list-group-item">
+                    <Row>
+                        <Col md={1}><h5>Bk</h5></Col>
+
+                        <Col md={4}>
+                            <Row><Col><Link to={`/block/${item.number}`}>{item.number}</Link></Col></Row>
+                            <Row><Col><small>{item.timediff} secs ago</small></Col></Row>
+                        </Col>
+
+                        <Col md={7}>
+                            <Row><Col><Link to={`/block/${item.number}`}>{item.hash.slice(0,22) + '...'}</Link></Col></Row>
+                            <Row><Col><small>{item.transactions.length} transaction(s)</small></Col></Row>
+                        </Col>
+
+                    </Row>
+                </ListGroup.Item>
+            ))}
+        </ListGroup>
     );
 };
 export default LatestBlocks;
