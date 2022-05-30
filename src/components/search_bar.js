be634922d8810c4c3ae11cf34d16829bbd30992d
Jocelyn <82773188+ragueneau@users.noreply.github.com>
2022-05-30 01:16:41 -0400

    Update search_bar.js

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Button } from 'react-bootstrap'
 
 class SearchBar extends Component {
     constructor(props) {
@@ -7,20 +8,37 @@ class SearchBar extends Component {
         this.state = { term: '' };
     }
 
+    search(term) {
+        console.log(`Searching for ${term}`);
+
+        //setState('');
+        //redirect to block page
+
+        //if the term is an integer, redirect to block page
+        //if the term is a string, redirect to token page
+
+
+
+    }
+
+    onInputChange(term) {
+        this.setState({ term });
+        //this.props.onSearchTermChange(term);
+
+    }
+
     render() {
         return (
             <div className="search-bar">
                 <input
                     value={this.state.term}
-                    onChange={event => this.onInputChange(event.target.value)}/>
+                    onChange={event => this.onInputChange(event.target.value)}
+                    onSubmit={event => this.search(this.state.term)}
+                /> <Button variant="outline-info" onClick={() => this.search(this.state.term )}>Search</Button>
             </div>
        );
     }
 
-    onInputChange(term) {
-        this.setState({ term });
-        this.props.onSearchTermChange(term);
-    }
 };
 
 export default SearchBar;
