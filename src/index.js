bd927e3ebcde17750c11341b124997ff1d53ca88
Jocelyn <82773188+ragueneau@users.noreply.github.com>
2022-06-08 12:57:24 -0400

    Package Update

--- src/index.js
+++ src/index.js
@@ -4,6 +4,16 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 
+//change website title
+document.title = "CoeptIX Etherscan";
+
+//change website favicon
+let link = document.createElement('link');
+link.type = 'image/x-icon';
+link.rel = 'shortcut icon';
+link.href = 'https://etherscan.io/images/favicon.ico';
+document.getElementsByTagName('head')[0].appendChild(link);
+
 const rootElement = document.getElementById("root");
 render( <App />, rootElement);
 
