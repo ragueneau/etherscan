2a0a18836f044b384a1baf957fe2185b07c5a43b
Jocelyn <82773188+ragueneau@users.noreply.github.com>
2022-06-24 19:25:09 -0400

    Update TokenList.js

--- src/components/TokenList.js
+++ src/components/TokenList.js
@@ -12,6 +12,7 @@ const TokenList = ({tokens}) => {
                     <th>Address</th>
                     <th>Decimals</th>
                     <th>Total Supply</th>
+                    <th>Wrapped</th>
                     <th>Actions</th>
                 </tr>
             </thead>
@@ -23,6 +24,7 @@ const TokenList = ({tokens}) => {
                         <td><Link to={`/address/${item.address}`}>{item.address.slice(0, 9) + '...'+item.address.slice(33, 42)}</Link></td>
                         <td>{item.decimals}</td>
                         <td>{item.supply / 10 ** 18 }</td>
+                        <td>{item.balance / 10 ** 18 }</td>
                         <td>
                             <Button variant="secondary btn-list" size="sm" as={Link} to={`/token/${item.address}`}>View
                             </Button> <Button variant="secondary btn-list" size="sm" as={Link} to={`/interface/${item.address}`}>Interface</Button>
