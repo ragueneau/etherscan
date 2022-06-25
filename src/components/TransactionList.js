b8f85cf839ee35ca8e1cd0917b5201b20e553bcb
Jocelyn <82773188+ragueneau@users.noreply.github.com>
2022-06-25 06:51:28 -0400

    Update TransactionList.js

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Card, Button, Table } from 'react-bootstrap'
 import { Link } from "react-router-dom"
+import { getAddress, linkAddress } from '../class/Tools'
+import { getProvider, isContract } from '../class/Evm'
 
 const TransactionList = ({txs}) => {
     return (
@@ -27,8 +29,8 @@ const TransactionList = ({txs}) => {
                                 <td><Link to={`/tx/${item.hash}`}>{item.hash ? item.hash.slice(0, 7) + '...': null}</Link></td>
                                 <td><Button variant="secondary btn-list" size="sm" className="ml-2">{item.method}</Button></td>
                                 <td><Link to={`/block/${item.blockNumber}`}>{item.blockNumber}</Link></td>
-                                <td><Link to={`/address/${item.from}`}>{item.from ? item.from.slice(0, 7) + '...'+item.from.slice(38, 42) : null}</Link></td>
-                                <td>{item.to ? <Link to={`/address/${item.to}`}>{item.to.slice(0, 7) + '...'+item.to.slice(38, 42)}</Link> : null}</td>
+                                <td><Link to={`/address/${item.from}`}>{item.from ? linkAddress(item.from) : null}</Link></td>
+                                <td>{item.to ? <Link to={`/address/${item.to}`}>{linkAddress(item.to)}</Link> : null}</td>
                                 <td>{item.value / 10 ** 18} ether</td>
                                 <td>{item.gas / 10 ** 0} gwei</td>
                             </tr>
