fe4b9afda40f3f086dd0c6880a66a7e4328fc590
Jocelyn <82773188+ragueneau@users.noreply.github.com>
2022-07-12 03:45:12 -0400

    Update Accounts.js

--- src/routes/Accounts.js
+++ src/routes/Accounts.js
@@ -2,7 +2,7 @@
 import Config from '../config.json'
 import { useState, useEffect } from 'react'
 import { ethers } from "ethers"
-import { Table, Card, Row, Col, Spinner, ListGroup } from 'react-bootstrap'
+import { Button, Table, Card, Row, Col, Spinner, ListGroup } from 'react-bootstrap'
 import TokenList from '../components/TokenList'
 import { Link } from "react-router-dom";
 
@@ -15,16 +15,38 @@ const Accounts = ({ networkName }) => {
     const [items, setItems] = useState([])
     const [stats, setStats] = useState({})
 
-    const [topBalances, setTopBalances] = useState([])
-    const [topMiners, setTopMiners] = useState([])
+    const [topBalances, setTopBalances] = useState([
+        {  name: '0x', balance: 0 },
+        {  name: '0x', balance: 0 },
+        {  name: '0x', balance: 0 },
+        {  name: '0x', balance: 0 },
+        {  name: '0x', balance: 0 },
+        {  name: '0x', balance: 0 },
+        {  name: '0x', balance: 0 },
+        {  name: '0x', balance: 0 },
+        {  name: '0x', balance: 0 },
+        {  name: '0x', balance: 0 }
+    ])
+    const [topMiners, setTopMiners] = useState([
+        {  name: '0x', blocks: 0, totalrewards: 0 },
+        {  name: '0x', blocks: 0, totalrewards: 0 },
+        {  name: '0x', blocks: 0, totalrewards: 0 },
+        {  name: '0x', blocks: 0, totalrewards: 0 },
+        {  name: '0x', blocks: 0, totalrewards: 0 },
+        {  name: '0x', blocks: 0, totalrewards: 0 },
+        {  name: '0x', blocks: 0, totalrewards: 0 },
+        {  name: '0x', blocks: 0, totalrewards: 0 },
+        {  name: '0x', blocks: 0, totalrewards: 0 },
+        {  name: '0x', blocks: 0, totalrewards: 0 }
+    ])
 
-    const [toptransactions, setTopTransactions] = useState([])
-    //const [toptransfers, setTopTransfers] = useState([])
-    //const [toptokens, setTopTokens] = useState([])
-    const [topaddresses, setTopAddresses] = useState([])
-    const [topcontracts, setTopContracts] = useState([])
-    //const [topevents, setTopEvents] = useState([])
-    //const [topblocksbyhash, setTopBlocksByHash] = useState([])
+    const [topAddressesTxns, setTopAddressesTxns] = useState([]);
+
+    const [topTransactions, setTopTransactions] = useState([])
+
+    const [topContracts, setTopContracts] = useState([])
+    const [topContractsTxns, setTopContractsTxns] = useState([])
+    const [topGasUsed, setTopGasUsed] = useState([])
 
     const getTopMiners = async () => {
         let stats = {}
@@ -88,12 +110,139 @@ const Accounts = ({ networkName }) => {
 
     }
 
- 
+    const getTopTransactions = async () => {
+        let stats = {}
+
+        //today date yyyy-mm-dd
+        const today = new Date()
+        const dd = String(today.getDate()).padStart(2, '0')
+        const mm = String(today.getMonth() + 1).padStart(2, '0')
+        const yyyy = today.getFullYear()
+        const todayDate = yyyy + '-' + mm + '-' + dd
+
+        //yesterday date yyyy-mm-dd
+        const yesterday = new Date(today)
+        yesterday.setDate(today.getDate() - 1)
+        const ydd = String(yesterday.getDate()).padStart(2, '0')
+        const ymm = String(yesterday.getMonth() + 1).padStart(2, '0')
+        const yyyy2 = yesterday.getFullYear()
+        const yesterdayDate = yyyy2 + '-' + ymm + '-' + ydd
+
+        stats = {
+            startdate: yesterdayDate,
+            enddate: todayDate
+        }
+        //get stats dailytxnfee
+        await axios.get(Config.restAPI + '/api?module=stats&action=topaddressestxns&apikey=' + Config.ApiKeyToken+'&startdate='+todayDate+'&enddate='+todayDate)
+        .then(function (response) {
+            stats.topTransactions = response.data.result
+            setTopTransactions(stats.topTransactions)
+        })
+    }
+
+    const getTopContracts = async () => {
+
+        let stats = {}
+
+        //today date yyyy-mm-dd
+        const today = new Date()
+        const dd = String(today.getDate()).padStart(2, '0')
+        const mm = String(today.getMonth() + 1).padStart(2, '0')
+        const yyyy = today.getFullYear()
+        const todayDate = yyyy + '-' + mm + '-' + dd
+
+        //yesterday date yyyy-mm-dd
+        const yesterday = new Date(today)
+        yesterday.setDate(today.getDate() - 1)
+        const ydd = String(yesterday.getDate()).padStart(2, '0')
+        const ymm = String(yesterday.getMonth() + 1).padStart(2, '0')
+        const yyyy2 = yesterday.getFullYear()
+        const yesterdayDate = yyyy2 + '-' + ymm + '-' + ydd
+
+        stats = {
+            startdate: yesterdayDate,
+            enddate: todayDate
+        }
+        //get stats dailytxnfee
+        await axios.get(Config.restAPI + '/api?module=stats&action=topcontracts&apikey=' + Config.ApiKeyToken+'&startdate='+todayDate+'&enddate='+todayDate)
+        .then(function (response) {
+            stats.topcontracts = response.data.result
+            setTopContracts(stats.topcontracts)
+        })
+
+    }
+
+    const getTopContractsTxns = async () => {
+
+        let stats = {}
+
+        //today date yyyy-mm-dd
+        const today = new Date()
+        const dd = String(today.getDate()).padStart(2, '0')
+        const mm = String(today.getMonth() + 1).padStart(2, '0')
+        const yyyy = today.getFullYear()
+        const todayDate = yyyy + '-' + mm + '-' + dd
+
+        //yesterday date yyyy-mm-dd
+        const yesterday = new Date(today)
+        yesterday.setDate(today.getDate() - 1)
+        const ydd = String(yesterday.getDate()).padStart(2, '0')
+        const ymm = String(yesterday.getMonth() + 1).padStart(2, '0')
+        const yyyy2 = yesterday.getFullYear()
+        const yesterdayDate = yyyy2 + '-' + ymm + '-' + ydd
+
+        stats = {
+            startdate: yesterdayDate,
+            enddate: todayDate
+        }
+        //get stats dailytxnfee
+        await axios.get(Config.restAPI + '/api?module=stats&action=topcontractstxns&apikey=' + Config.ApiKeyToken+'&startdate='+todayDate+'&enddate='+todayDate)
+        .then(function (response) {
+            stats.topcontractstxns = response.data.result
+            setTopContractsTxns(stats.topcontractstxns)
+        })
+
+    }
+
+    const getTopGasUsed = async () => {
+        let stats = {}
+
+        //today date yyyy-mm-dd
+        const today = new Date()
+        const dd = String(today.getDate()).padStart(2, '0')
+        const mm = String(today.getMonth() + 1).padStart(2, '0')
+        const yyyy = today.getFullYear()
+        const todayDate = yyyy + '-' + mm + '-' + dd
+
+        //yesterday date yyyy-mm-dd
+        const yesterday = new Date(today)
+        yesterday.setDate(today.getDate() - 1)
+        const ydd = String(yesterday.getDate()).padStart(2, '0')
+        const ymm = String(yesterday.getMonth() + 1).padStart(2, '0')
+        const yyyy2 = yesterday.getFullYear()
+        const yesterdayDate = yyyy2 + '-' + ymm + '-' + ydd
+
+        stats = {
+            startdate: yesterdayDate,
+            enddate: todayDate
+        }
+        //get stats dailytxnfee
+        await axios.get(Config.restAPI + '/api?module=stats&action=topgasused&apikey=' + Config.ApiKeyToken+'&startdate='+todayDate+'&enddate='+todayDate)
+        .then(function (response) {
+            stats.topgasused = response.data.result
+            setTopGasUsed(stats.topgasused)
+        })
+    }
+
     useEffect(() => {
         let timer = setTimeout(() => {
 
             getTopMiners()
             getTopBalances()
+            getTopContracts()
+            getTopContractsTxns()
+            getTopTransactions()
+            getTopGasUsed()
 
             setLoading(false)
         }, 1000);
@@ -115,6 +264,7 @@ const Accounts = ({ networkName }) => {
                                 <Table size="sm">
                                     <thead>
                                         <tr>
+                                            <th>#</th>
                                             <th>Account</th>
                                             <th>Origin</th>
                                             <th>Balance</th>
@@ -123,8 +273,9 @@ const Accounts = ({ networkName }) => {
                                     <tbody>
                                     {topBalances && topBalances.map((item, index) => (
                                         <tr key={index}>
-                                            <td>{item.name ? (<Link title={item.address} to={`/address/${item.address}`}>{item.name}</Link>) : (<Link label={item.address} to={`/address/${item.address}`}>{linkAddress(item.address)}</Link>)}</td>
-                                            <td><Link label={item.blocknumber} to={`/block/${item.blocknumber}`}>{item.blocknumber}</Link></td>
+                                            <td>{index + 1}</td>
+                                            <td>{item.name ? (<Link title={item.address} to={`/address/${item.address}`}>{item.name}</Link>) : (<span>{linkAddress(item.address)}</span>)}</td>
+                                            <td><Link title={item.blocknumber} to={`/block/${item.blocknumber}`}>{item.blocknumber}</Link></td>
                                             <td>{item.balance/1000000000000000000} xEth</td>
                                         </tr>
                                     ))}
@@ -140,6 +291,26 @@ const Accounts = ({ networkName }) => {
                                 <Card.Title >Top Transactions</Card.Title>
                             </Card.Header>
                             <Card.Body className="std-card-info-body">
+                                <Table size="sm">
+                                    <thead>
+                                        <tr>
+                                            <th>#</th>
+                                            <th>Addresses</th>
+                                            <th>Txns Count</th>
+                                            <th>Value</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                    {topTransactions && topTransactions.map((item, index) => (
+                                        <tr key={index}>
+                                            <td>{index + 1}</td>
+                                            <td>{item.name ? (<Link title={item.address} to={`/address/${item.address}`}>{item.name}</Link>) : (<span>{linkAddress(item.address)}</span>)}</td>
+                                            <td>{item.txnscount}</td>
+                                            <td>    </td>
+                                        </tr>
+                                    ))}
+                                    </tbody>
+                                </Table>
                             </Card.Body>
                         </Card>
                     </Col>
@@ -157,6 +328,7 @@ const Accounts = ({ networkName }) => {
                                 <Table size="sm">
                                     <thead>
                                         <tr>
+                                            <th>#</th>
                                             <th>Miner</th>
                                             <th>Blocks</th>
                                             <th>Total Rewards</th>
@@ -165,7 +337,8 @@ const Accounts = ({ networkName }) => {
                                     <tbody>
                                     {topMiners && topMiners.map((item, index) => (
                                         <tr key={index}>
-                                            <td>{item.name ? (<Link label={item.miner} to={`/address/${item.miner}`}>{item.name}</Link>) : (<Link label={item.miner} to={`/address/${item.miner}`}>{linkAddress(item.miner)}</Link>)}</td>
+                                            <td>{index + 1}</td>
+                                            <td>{item.name ? (<Link title={item.miner} to={`/address/${item.miner}`}>{item.name}</Link>) : (<span>{linkAddress(item.miner)}</span>)}</td>
                                             <td>{item.blocks}</td>
                                             <td>{item.totalrewards/1000000000} xEth</td>
                                         </tr>
@@ -182,6 +355,26 @@ const Accounts = ({ networkName }) => {
                                 <Card.Title >Top Gas Used</Card.Title>
                             </Card.Header>
                             <Card.Body className="std-card-info-body">
+                                <Table size="sm">
+                                    <thead>
+                                        <tr>
+                                            <th>#</th>
+                                            <th>Addresses</th>
+                                            <th>Txns Count</th>
+                                            <th>Value</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                    {topGasUsed && topGasUsed.map((item, index) => (
+                                        <tr key={index}>
+                                            <td>{index + 1}</td>
+                                            <td>{item.name ? (<Link title={item.address} to={`/address/${item.address}`}>{item.name}</Link>) : (<span>{linkAddress(item.address)}</span>)}</td>
+                                            <td>{item.txnscount}</td>
+                                            <td>    </td>
+                                        </tr>
+                                    ))}
+                                    </tbody>
+                                </Table>
                             </Card.Body>
                         </Card>
                     </Col>
@@ -192,18 +385,62 @@ const Accounts = ({ networkName }) => {
                 <Col md={6}>
                     <Card className="std-card-info">
                             <Card.Header>
-                                <Card.Title>Top Contract by Value</Card.Title>
+                                <Card.Title>Top Contracts by Value</Card.Title>
                             </Card.Header>
                             <Card.Body className="std-card-info-body">
+                                <Table size="sm">
+                                    <thead>
+                                        <tr>
+                                            <th>#</th>
+                                            <th>Contracts</th>
+                                            <th>Origin</th>
+                                            <th>Total Wrapped xEth</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                    {topContracts && topContracts.map((item, index) => (
+                                        <tr key={index}>
+                                            <td>{index + 1}</td>
+                                            <td>{item.name ? (<Link title={item.address} to={`/address/${item.address}`}>{item.name}</Link>) : (<span>{linkAddress(item.address)}</span>)}</td>
+                                            <td><Link title={item.blocknumber} to={`/block/${item.blocknumber}`}>{item.blocknumber}</Link></td>
+                                            <td>{item.balance/1000000000000000000} xEth</td>
+                                        </tr>
+                                    ))}
+                                    </tbody>
+                                </Table>
                             </Card.Body>
                         </Card>
                     </Col>
                     <Col md={6}>
                     <Card className="std-card-info">
                             <Card.Header>
-                                <Card.Title>Top Contract by Tx</Card.Title>
+                                <Card.Title>Top Contracts by Tx</Card.Title>
                             </Card.Header>
                             <Card.Body className="std-card-info-body">
+                                <Table size="sm">
+                                    <thead>
+                                        <tr>
+                                            <th>#</th>
+                                            <th>Contracts</th>
+                                            <th>Txns Count</th>
+                                            <th>Value</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                    {topContractsTxns && topContractsTxns.map((item, index) => (
+                                        <tr key={index}>
+                                            <td>{index + 1}</td>
+                                            <td>{item.name ? (<Link title={item.address} to={`/address/${item.address}`}>{item.name}</Link>) : (<span>{linkAddress(item.address)}</span>)}
+
+                                            {item.erctype ? (<Button variant="outline-primary" size="sm" >{item.erctype}</Button>) : (<span></span>)}
+
+                                            </td>
+                                            <td>{item.txnscount}</td>
+                                            <td>    </td>
+                                        </tr>
+                                    ))}
+                                    </tbody>
+                                </Table>
                             </Card.Body>
                         </Card>
                     </Col>
